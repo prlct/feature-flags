7e355b9b2c223ac0d72e5ea456c0714b51ea2c95
Yegor K <e.koren@paralect.com>
2022-08-23 17:02:24 +0300

    Add User resource on the back-end

---
+++ api/src/resources/user/user.types.ts
@@ -0,0 +1,10 @@
+export type User = {
+  _id: string;
+  applicationId: string;
+  sessionId: string;
+  externalId: string;
+  email: string;
+  fullName: string;
+  createdOn: string;
+  updatedOn: string;
+};
