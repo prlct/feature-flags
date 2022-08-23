713d8f16592850db35ad9ad01f944ecefba4f290
Yegor K <e.koren@paralect.com>
2022-08-23 18:32:10 +0300

    Add Private API Key display in the admin front-end

--- web/src/pages/api-key/index.page.jsx
+++ web/src/pages/api-key/index.page.jsx
@@ -5,23 +5,38 @@ import {
   Stack,
   Loader,
   TextInput,
+  Group,
   Tooltip,
   ActionIcon,
 } from '@mantine/core';
-import { IconCopy, IconCheck } from '@tabler/icons';
+import { IconCopy, IconCheck, IconEye, IconEyeOff } from '@tabler/icons';
 import { applicationApi } from 'resources/application';
 
 const ApiKey = () => {
-  const [isCopied, setIsCopied] = useState(false);
+  const [isPublicKeyCopied, setIsPublicKeyCopied] = useState(false);
+  const [isPrivateKeyCopied, setIsPrivateKeyCopied] = useState(false);
+  const [isPrivateKeyVisible, setIsPrivateKeyVisible] = useState(false);
   const { data, isLoading } = applicationApi.useGetApplication();
 
-  const handleCopy = useCallback(() => {
+  const handlePublicKeyCopy = useCallback(() => {
     navigator.clipboard.writeText(data?.publicApiKey);
-    setIsCopied(true);
+    setIsPublicKeyCopied(true);
     setTimeout(() => {
-      setIsCopied(false);
+      setIsPublicKeyCopied(false);
     }, 2000);
-  }, []);
+  }, [data?.publicApiKey]);
+
+  const handlePrivateKeyCopy = useCallback(() => {
+    navigator.clipboard.writeText(data?.privateApiKey);
+    setIsPrivateKeyCopied(true);
+    setTimeout(() => {
+      setIsPrivateKeyCopied(false);
+    }, 2000);
+  }, [data?.privateApiKey]);
+
+  const handlePrivateKeyVisible = useCallback(() => {
+    setIsPrivateKeyVisible(!isPrivateKeyVisible);
+  }, [isPrivateKeyVisible]);
 
   return (
     <>
@@ -30,19 +45,40 @@ const ApiKey = () => {
       </Head>
       {isLoading ? <Loader /> : (
         <Stack spacing="lg">
-          <Title order={2}>API key</Title>
+          <Title order={2}>API keys</Title>
           <TextInput
-            sx={{ width: 680 }}
-            label="Publishable API key"
+            sx={{ width: 560 }}
+            label={<Title order={4}>Publishable API key</Title>}
             value={data?.publicApiKey}
-            disabled
             rightSection={
-              <Tooltip label={isCopied ? 'Copied' : 'Copy'} withArrow position="right">
-                <ActionIcon color={isCopied ? 'teal' : 'gray'} onClick={handleCopy}>
-                  {isCopied ? <IconCheck size={16} /> : <IconCopy size={16} />}
+              <Tooltip label={isPublicKeyCopied ? 'Copied' : 'Copy'} withArrow position="right">
+                <ActionIcon color={isPublicKeyCopied ? 'teal' : 'gray'} onClick={handlePublicKeyCopy}>
+                  {isPublicKeyCopied ? <IconCheck size={16} /> : <IconCopy size={16} />}
                 </ActionIcon>
               </Tooltip>
             }
+            onChange={() => {}}
+          />
+          <TextInput
+            sx={{ width: 560 }}
+            label={<Title order={4}>Private API key</Title>}
+            value={isPrivateKeyVisible ? data?.privateApiKey : '*******************************************************'}
+            rightSectionWidth={80}
+            rightSection={
+              <Group>
+                <Tooltip label={isPrivateKeyVisible ? 'Hide' : 'Show'} withArrow position="right">
+                  <ActionIcon onClick={handlePrivateKeyVisible}>
+                    {isPrivateKeyVisible ? <IconEyeOff size={16} /> : <IconEye size={16} />}
+                  </ActionIcon>
+                </Tooltip>
+                <Tooltip label={isPrivateKeyCopied ? 'Copied' : 'Copy'} withArrow position="right">
+                <ActionIcon color={isPrivateKeyCopied ? 'teal' : 'gray'} onClick={handlePrivateKeyCopy}>
+                    {isPrivateKeyCopied ? <IconCheck size={16} /> : <IconCopy size={16} />}
+                  </ActionIcon>
+                </Tooltip>
+              </Group>
+            }
+            onChange={() => {}}
           />
         </Stack>
       )}
