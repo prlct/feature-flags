361373462a8789479ce264bc3448a7974eb09e64
Yegor K <e.koren@paralect.com>
2022-08-23 17:09:56 +0300

    Fix signup process on the back-end

--- api/src/resources/application/application.types.ts
+++ api/src/resources/application/application.types.ts
@@ -2,6 +2,8 @@ export type Application = {
   _id: string;
   companyId: string;
   publicApiKey: string;
+  privateApiKey: string;
+  featureIds: string[];
   createdOn: string;
   updatedOn: string;
   deletedOn?: string;
