d08a82054358e1947a57f91dee833b1903bb26e3
Yegor K <e.koren@paralect.com>
2022-08-31 17:59:30 +0300

    Add typescript to js-sdk

--- packages/js-sdk/src/api.service.ts
+++ packages/js-sdk/src/api.service.ts
@@ -1,6 +1,5 @@
-// eslint-disable-next-line max-classes-per-file
-const axios = require('axios');
-const qs = require('qs');
+import axios, { AxiosRequestConfig } from 'axios';
+import * as qs from 'qs';
 
 const DEV_API_URL = 'http://localhost:3001';
 const PROD_API_URL = 'https://api.growthflags.com';
@@ -8,7 +7,12 @@ const PROD_API_URL = 'https://api.growthflags.com';
 const API_URL = PROD_API_URL;
 
 class ApiError extends Error {
-  constructor(data, status = 500, statusText = 'Internal Server Error') {
+  name: string;
+  data: unknown;
+  status: number;
+  __proto__: unknown;
+  
+  constructor(data: unknown, status: number = 500, statusText: string = 'Internal Server Error') {
     super(`${status} ${statusText}`);
 
     this.constructor = ApiError;
@@ -27,19 +31,27 @@ class ApiError extends Error {
     return this.stack;
   }
 }
-const throwApiError = ({ status, statusText, data }) => {
+const throwApiError = ({ status, statusText, data }: { status: number; statusText: string; data: unknown}) => {
   console.error(`API Error: ${status} ${statusText}`, data); //eslint-disable-line
   throw new ApiError(data, status, statusText);
 };
 
-class ApiClient {
-  constructor(axiosConfig) {
-    this._handlers = new Map();
+interface ErrorResponse {
+  status: number;
+  statusText: string;
+  data: unknown;
+}
 
+class ApiClient {
+  _handlers: any;
+  _api: any;
+
+  constructor(axiosConfig: AxiosRequestConfig<any>) {
+    this._handlers = new Map();
     this._api = axios.create(axiosConfig);
     this._api.interceptors.response.use(
-      (response) => response.data,
-      (error) => {
+      (response: { data: unknown }) => response.data,
+      (error: { response: ErrorResponse; code: number; message: string; data: unknown }) => {
         if (axios.isCancel(error)) {
           throw error;
         }
@@ -52,7 +64,7 @@ class ApiClient {
         };
 
         const errorHandlers = this._handlers.get('error') || [];
-        errorHandlers.forEach((handler) => {
+        errorHandlers.forEach((handler: any) => {
           handler(errorResponse);
         });
 
@@ -61,7 +73,7 @@ class ApiClient {
     );
   }
 
-  get(url, params = {}, requestConfig = {}) {
+  get(url: string, params = {}, requestConfig = {}) {
     return this._api({
       method: 'get',
       url,
@@ -70,7 +82,7 @@ class ApiClient {
     });
   }
 
-  post(url, data = {}, requestConfig = {}) {
+  post(url: string, data = {}, requestConfig = {}) {
     return this._api({
       method: 'post',
       url,
@@ -79,7 +91,7 @@ class ApiClient {
     });
   }
 
-  put(url, data = {}, requestConfig = {}) {
+  put(url: string, data = {}, requestConfig = {}) {
     return this._api({
       method: 'put',
       url,
@@ -88,7 +100,7 @@ class ApiClient {
     });
   }
 
-  delete(url, data = {}, requestConfig = {}) {
+  delete(url: string, data = {}, requestConfig = {}) {
     return this._api({
       method: 'delete',
       url,
@@ -97,7 +109,7 @@ class ApiClient {
     });
   }
 
-  on(event, handler) {
+  on(event: unknown, handler: unknown) {
     if (this._handlers.has(event)) {
       this._handlers.get(event).add(handler);
     } else {
@@ -108,7 +120,7 @@ class ApiClient {
   }
 }
 
-module.exports = new ApiClient({
+export default new ApiClient({
   baseURL: API_URL,
   withCredentials: true,
   responseType: 'json',
