3a6c8dd8e4b51948f73319c9820bbbc6e5a35036
igor <ionovitsky@gmail.com>
2022-09-26 15:54:50 +0300

    Add payment success modal

---
+++ web/src/pages/home/components/payment-sucess-modal/index.jsx
@@ -0,0 +1,96 @@
+import { useState, useEffect, useCallback, useMemo } from 'react';
+import { useRouter } from 'next/router';
+import Confetti from 'react-confetti'
+import {
+  Badge,
+  Container,
+  Modal,
+  Stack,
+  Space,
+  Text,
+  Title,
+} from '@mantine/core';
+import { IconCheck } from '@tabler/icons';
+
+import * as routes from 'routes';
+import subscriptionList from 'pages/subscription-plans/subscription-list';
+
+import { useStyles } from './styles';
+
+const PaymentSuccessModal = () => {
+  const { classes } = useStyles();
+  const router = useRouter()
+
+  const [opened, setOpened] = useState(false);
+  const [activeSubscriptionPlan, setActiveSubscriptionPlan] = useState();
+
+  const onClose = useCallback(() => {
+    setOpened(false);
+    router.replace(routes.route.home, undefined, { shallow: true });
+  }, []);
+
+  useEffect(() => {
+    if (router.query.subscriptionPlan) {
+      setOpened(true);
+      setActiveSubscriptionPlan(subscriptionList.find((item) => item.id === router.query.subscriptionPlan));
+    }
+  }, [router.query.subscriptionPlan]);
+
+  const renderFeatureList = useCallback(() =>
+    activeSubscriptionPlan.features.map((item, index) => (
+      <Container
+        key={index}
+        fluid
+        sx={{
+          display: 'flex',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          width: '100%',
+          padding: 0,
+        }}
+      >
+        <Badge
+          variant="filled"
+          classNames={{
+            root: classes.badgeContainer,
+            inner: classes.badgeInner,
+          }}
+        >
+          <IconCheck size={18} />
+        </Badge>
+        <Space w={8} />
+        {item}
+      </Container>
+    )),
+    [activeSubscriptionPlan]
+  );
+
+  return (
+    <>
+      <Modal
+        centered
+        title={
+          <Title order={3}>
+            Success
+          </Title>
+        }
+        opened={opened}
+        onClose={onClose}
+      >
+        <Text>You have successfully upgraded your plan.</Text>
+        {!!activeSubscriptionPlan && (
+          <>
+            <Text>List of available features:</Text>
+            <Space h={16} />
+            <Stack>
+              {renderFeatureList()}
+            </Stack>
+          </>
+        )}
+      </Modal>
+      {opened && <Confetti />}
+    </>
+  )
+};
+
+export default PaymentSuccessModal;
