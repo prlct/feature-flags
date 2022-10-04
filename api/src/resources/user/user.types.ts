7d19bb1d3a1b6d9faf7610aaa282e0582743b8e3
Elena Vlasova <e.vlasova@paralect.com>
2022-10-04 15:25:25 +0200

    Fix email in the user type

--- api/src/resources/user/user.types.ts
+++ api/src/resources/user/user.types.ts
@@ -3,7 +3,7 @@ import { Env } from 'resources/application';
 export type User = {
   _id: string;
   applicationId: string;
-  email: string;
+  email?: string;
   externalId: string;
   env: Env,
   data: { [key: string]: any };
