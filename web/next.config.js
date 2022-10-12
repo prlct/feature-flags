d0c11338c1efab543423eac3bfa13f73d711ba2c
Vladimir Shevtsov <v.shevtsov@paralect.com>
2022-10-12 15:19:31 +0300

    Change urls to test env-t

--- web/next.config.js
+++ web/next.config.js
@@ -13,4 +13,11 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_MAGIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_MAGIC_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_GROWTHFLAGS_PUBLIC_KEY: process.env.NEXT_PUBLIC_GROWTHFLAGS_PUBLIC_KEY,
+    NEXT_PUBLIC_APP_ENV: process.env.NEXT_PUBLIC_APP_ENV,
+    NEXT_PUBLIC_CRISP_WEBSITE_ID: process.env.NEXT_PUBLIC_CRISP_WEBSITE_ID,
+    NEXT_PUBLIC_HOTJAR_ID: process.env.NEXT_PUBLIC_HOTJAR_ID,
+  },
 };
