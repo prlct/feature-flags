36c132020dc404e21acfda60f698bd847858a834
Veronika Ulyanchik <84153492+VeronikaUlyanchik@users.noreply.github.com>
2022-10-26 15:40:59 +0300

    Implement google auth (#78)
    * Implement google auth
    
    * Remove secrets
    
    * Remove secrets 2

--- api/src/services/index.ts
+++ api/src/services/index.ts
@@ -1,6 +1,7 @@
 import authService from './auth/auth.service';
 import cloudStorageService from './cloud-storage/cloud-storage.service';
 import emailService from './email/email.service';
+import googleService from './google/google.service';
 import socketService from './socket/socket.service';
 
 export {
@@ -8,4 +9,5 @@ export {
   cloudStorageService,
   emailService,
   socketService,
+  googleService,
 };
