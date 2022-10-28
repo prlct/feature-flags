5026089dc2f56e2fb53f65b7d0099f48029ee30b
VeronikaUlyanchik <veronikaullll1998@gmail.com>
2022-10-28 10:31:26 +0300

    Fix url

--- api/src/services/github/github.service.ts
+++ api/src/services/github/github.service.ts
@@ -1,11 +1,13 @@
-import config from 'config';
 import axios from 'axios';
 import { Octokit } from 'octokit';
+
+import config from 'config';
 
 const urlParams = {
   gitHub:'https://github.com/login/oauth',
   client_id: config.github.clientId,
   redirect_uri: `${config.apiUrl}/account/sign-in/github`,
+  client_secret: config.github.clientSecret,
 };
 
 const oAuthURL = `${urlParams.gitHub}/authorize?client_id=${urlParams.client_id}&redirect_uri=${urlParams.redirect_uri}&scope=user`;
@@ -13,14 +15,8 @@ const oAuthURL = `${urlParams.gitHub}/authorize?client_id=${urlParams.client_id}
 const exchangeCodeForToken = async (code: string) => {
   try {
     const { data } = await axios.request({
-      url: `${urlParams.gitHub}/access_token?client_id=${config.github.clientId}&client_secret=${config.github.clientSecret}&code=${code}&redirect_uri=${config.apiUrl}/account/sign-in/github&scope=user`,
+      url: `${urlParams.gitHub}/access_token?client_id=${urlParams.client_id}&client_secret=${urlParams.client_secret}&code=${code}&redirect_uri=${urlParams.redirect_uri}&scope=user`,
       method: 'post',
-      params: {
-        client_id: config.github.clientId,
-        redirect_uri: `${config.apiUrl}/account/sign-in/github`,
-        scope: 'user',
-        code,
-      },
       headers: {
         accept: 'application/json',
         'content-type': 'application/json',
@@ -31,31 +27,27 @@ const exchangeCodeForToken = async (code: string) => {
       auth: data.access_token,
     });
 
-    const { data : gitHubUser }  = await octokit.request('GET /user', {});
+    const { data : { name, email } }  = await octokit.request('GET /user', {});
+    let primaryEmail = email;
 
-    if (gitHubUser) {
-      const { name, email } = gitHubUser;
-      let primaryEmail = email;
+    if (!email) {
+      const emails = await octokit.request('GET /user/emails', {});
 
-      if (!email) {
-        const emails = await octokit.request('GET /user/emails', {});
-
-        primaryEmail = emails.data.find((em)=> em.primary)?.email || '';
-      }
-
-      const [givenName, familyName] = name?.split(' ') || ['User', 'User'];
-
-      const userInfo = {
-        given_name: givenName,
-        family_name: familyName,
-        email: primaryEmail,
-      };
-
-      return {
-        isValid: true,
-        payload: userInfo,
-      };
+      primaryEmail = emails.data.find((em)=> em.primary)?.email || '';
     }
+
+    const [givenName, familyName] = name?.split(' ') || ['User', 'User'];
+
+    const userInfo = {
+      given_name: givenName,
+      family_name: familyName,
+      email: primaryEmail,
+    };
+
+    return {
+      isValid: true,
+      payload: userInfo,
+    };
   } catch ({ message, ...rest }) {
     return {
       isValid: false,
