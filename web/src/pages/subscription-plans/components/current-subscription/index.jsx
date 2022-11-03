5ebde07ca8052e32b3137c8c4632546ae8dcd79d
VeronikaUlyanchik <veronikaullll1998@gmail.com>
2022-11-03 18:05:34 +0300

    Add start and end date

--- web/src/pages/subscription-plans/components/current-subscription/index.jsx
+++ web/src/pages/subscription-plans/components/current-subscription/index.jsx
@@ -1,13 +1,15 @@
 import { useCallback, memo } from 'react';
+import dayjs from 'dayjs';
 import PropTypes from 'prop-types';
+
 import {
   Button,
   Text,
+  Stack,
 } from '@mantine/core';
-
 import { subscriptionApi } from 'resources/subscription';
 
-const CurrentSubscriptionBlock = ({ onCancelSubscription }) => {
+const CurrentSubscriptionBlock = ({ onCancelSubscription, currentSubscription }) => {
   const cancelMutation = subscriptionApi.useCancelMutation();
 
   const onCancelCurrentSubscription = useCallback(() => {
@@ -21,6 +23,18 @@ const CurrentSubscriptionBlock = ({ onCancelSubscription }) => {
   return (
     <>
       <Text size="lg" align="center" sx={{ flex: '1 1 100%' }}>Cancel Subscription</Text>
+      <Stack>
+        <Text size="sm">
+          Your subscription started on
+          {' '}
+          {dayjs(currentSubscription.startDate).format('MMM DD, YYYY')}
+          {' '}
+          and expires
+          {' '}
+          {dayjs(currentSubscription.endDate).format('MMM DD, YYYY')}
+          .
+        </Text>
+      </Stack>
       <Text size="sm" sx={{ flex: '1 1 100%' }}>Cancelling your team subscription will affect members access to Growthflags.</Text>
 
       <Button variant="light" color="red" onClick={onCancelCurrentSubscription}>Cancel</Button>
@@ -30,6 +44,10 @@ const CurrentSubscriptionBlock = ({ onCancelSubscription }) => {
 
 CurrentSubscriptionBlock.propTypes = {
   onCancelSubscription: PropTypes.func.isRequired,
+  currentSubscription: PropTypes.shape({
+    startDate: PropTypes.instanceOf(Date).isRequired,
+    endDate: PropTypes.instanceOf(Date).isRequired,
+  }).isRequired,
 };
 
 export default memo(CurrentSubscriptionBlock);
