2552d096817ed820e37e0df97a67e1f8db1a8b60
Vladimir Shevtsov <v.shevtsov@paralect.com>
2022-11-16 13:10:47 +0300

    Fix trigger selection modal

--- web/src/components/emailEditor/emailEditor.jsx
+++ web/src/components/emailEditor/emailEditor.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
-import { useStyles } from './emailEditor.styles';
 import clsx from 'clsx';
 import { InputBase } from '@mantine/core';
 import dynamic from 'next/dynamic';
-const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+import { useStyles } from './emailEditor.styles';
 import 'react-quill/dist/quill.snow.css';
 
-RegExp.escape = (s) => {
-  return s.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
-};
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+
+RegExp.escape = (s) => s.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
 
 const formats = [
   'bold', 'italic', 'underline', 'link',
@@ -17,67 +16,64 @@ const formats = [
   'indent', 'align',
 ];
 
-function EmailEditor() {
+const EmailEditor = () => {
   const { classes } = useStyles();
   const [value, setValue] = useState('');
 
-  const editorId = React.useMemo(() => `q${nanoid()}`);
-  const modules = React.useMemo(() => {
-    return {
-      toolbar: `#${editorId}`,
-      clipboard: {
-        matchVisual: false,
-      },
-    };
-  });
+  const editorId = React.useMemo(() => `q${nanoid()}`, []);
+  const modules = React.useMemo(() => ({
+    toolbar: `#${editorId}`,
+    clipboard: {
+      matchVisual: false,
+    },
+  }), [editorId]);
 
   return (
     <div className={classes.root}>
-        <div className={classes.subject}>
-          <div className={classes.subjectInputWrap}>
-            <InputBase
-              required
+      <div className={classes.subject}>
+        <div className={classes.subjectInputWrap}>
+          <InputBase
+            required
               // value={subject}
-              className={classes.subjectInput}
-              placeholder="Subject"
-              variant="outlined"
-              minLength="1"
-            />
-          </div>
-        </div>
-        <div className={classes.quillWrap}>
-          <ReactQuill
-            theme="snow"
-            // value={value}
-            // onChange={setValue}
-            placeholder='Body'
-            formats={formats}
-            modules={modules}
-            style={{
-              minHeight: 300,
-            }}
+            className={classes.subjectInput}
+            placeholder="Subject"
+            variant="outlined"
+            minLength="1"
           />
         </div>
-        <div className={classes.separator} />
-        <div className={classes.toolbar}>
-          <div id={editorId} className={clsx(classes.quillControls, 'email-editor')}>
-            <button type="button" className="ql-bold" />
-            <button type="button" className="ql-italic" />
-            <button type="button" className="ql-underline" />
-            <button type="button" className="ql-link" />
-            <button type="button" className="ql-list" value="ordered" />
-            <button type="button" className="ql-list" value="bullet" />
-            <button type="button" className="ql-indent" value="-1" />
-            <button type="button" className="ql-indent" value="+1" />
-            <button type="button" className="ql-align" value="" />
-            <button type="button" className="ql-align" value="center" />
-            <button type="button" className="ql-align" value="right" />
-            <button type="button" className="ql-align" value="justify" />
-          </div>
+      </div>
+      <div className={classes.quillWrap}>
+        <ReactQuill
+          theme="snow"
+            // value={value}
+            // onChange={setValue}
+          placeholder="Body"
+          formats={formats}
+          modules={modules}
+          style={{
+            minHeight: 300,
+          }}
+        />
+      </div>
+      <div className={classes.separator} />
+      <div className={classes.toolbar}>
+        <div id={editorId} className={clsx(classes.quillControls, 'email-editor')}>
+          <button type="button" className="ql-bold" />
+          <button type="button" className="ql-italic" />
+          <button type="button" className="ql-underline" />
+          <button type="button" className="ql-link" />
+          <button type="button" className="ql-list" value="ordered" />
+          <button type="button" className="ql-list" value="bullet" />
+          <button type="button" className="ql-indent" value="-1" />
+          <button type="button" className="ql-indent" value="+1" />
+          <button type="button" className="ql-align" value="" />
+          <button type="button" className="ql-align" value="center" />
+          <button type="button" className="ql-align" value="right" />
+          <button type="button" className="ql-align" value="justify" />
         </div>
       </div>
+    </div>
   );
-}
+};
 
 export default EmailEditor;
-
--- web/src/pages/email-editor/index.page.jsx
+++ web/src/pages/email-editor/index.page.jsx
@@ -2,21 +2,19 @@ import Head from 'next/head';
 import {
   Stack,
 } from '@mantine/core';
-import EmailEditor from 'components/emailEditor'
+import EmailEditor from 'components/emailEditor';
 
-const EmailEditorPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Email editor</title>
-      </Head>
-      <Stack spacing="lg">
-        <Stack sx={{ width: 700 }}>
-          <EmailEditor />
-        </Stack>
+const EmailEditorPage = () => (
+  <>
+    <Head>
+      <title>Email editor</title>
+    </Head>
+    <Stack spacing="lg">
+      <Stack sx={{ width: 700 }}>
+        <EmailEditor />
       </Stack>
-    </>
-  );
-};
+    </Stack>
+  </>
+);
 
 export default EmailEditorPage;
