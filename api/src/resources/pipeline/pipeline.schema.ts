af5b6f377b351e11bdb647909e4f9bf40f3c049c
Vladimir Shevtsov <v.shevtsov@paralect.com>
2022-11-23 18:06:31 +0300

    Add pipeline creation endpoint

--- api/src/resources/pipeline/pipeline.schema.ts
+++ api/src/resources/pipeline/pipeline.schema.ts
@@ -1,9 +1,11 @@
 import Joi from 'joi';
+import { Env } from '../application';
 
 const schema = Joi.object({
   id: Joi.string().required(),
   applicationId: Joi.string().required(),
   name: Joi.string().required(),
+  env: Joi.string().valid(...Object.values(Env)),
 
   createdOn: Joi.date(),
   updatedOn: Joi.date(),
