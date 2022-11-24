33af7be04339a9f45101cb0fbbf9a5bc4d9e8fd3
Vladimir Shevtsov <v.shevtsov@paralect.com>
2022-11-24 13:35:11 +0300

    Fix pipeline/sequence creation

--- api/src/resources/application/application.routes.ts
+++ api/src/resources/application/application.routes.ts
@@ -3,9 +3,13 @@ import { routeUtil } from 'utils';
 import createFeature from './actions/create-feature';
 import getApplication from './actions/get-application';
 import getFeatures from './actions/get-features';
+import createPipeline from './actions/create-pipeline';
+import createSequence from './actions/create-sequence';
 
 const privateRoutes = routeUtil.getRoutes([
   createFeature,
+  createPipeline,
+  createSequence,
   getApplication,
   getFeatures,
 ]);
--- api/src/resources/pipeline/pipeline.routes.ts
+++ api/src/resources/pipeline/pipeline.routes.ts
@@ -1,9 +1,8 @@
 import { routeUtil } from 'utils';
 
-import createPipeline from './actions/create-pipeline';
 
 const privateRoutes = routeUtil.getRoutes([
-  createPipeline,
+
 ]);
 
 export default {
