ef72f61c86ff2c446e763b791b9a034018fd89d7
Vladimir Shevtsov <v.shevtsov@paralect.com>
2022-11-24 19:00:09 +0300

    Simplify sequence access middleware

--- api/src/resources/sequence/middlewares/sequence-access.ts
+++ api/src/resources/sequence/middlewares/sequence-access.ts
@@ -1,26 +1,19 @@
-import { includes } from 'lodash';
 import { AppKoaContext, Next } from 'types';
 import sequenceService from '../sequence.service';
 
 const sequenceAccess = async (ctx: AppKoaContext, next: Next) => {
   const { sequenceId } = ctx.params;
-  const sequence = await sequenceService.findOne({ _id: sequenceId });
+  const sequence = await sequenceService.findOne({
+    _id: sequenceId,
+    applicationId: { $in: ctx.state.admin.applicationIds },
+    deletedOn: { $exists: false },
+  });
 
   if (!sequence) {
-    ctx.status = 404;
-    ctx.body = {};
-    return;
-  }
-
-
-  const isAdminHasAccessToApplication = includes(ctx.state.admin.applicationIds, sequence.applicationId);
-
-  if (!isAdminHasAccessToApplication) {
     ctx.status = 403;
     ctx.body = {};
     return;
   }
-
 
   return next();
 };
