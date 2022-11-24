eeb4dfbe63b9a11fc1391c23222d5ecb8506b82b
Vladimir Shevtsov <v.shevtsov@paralect.com>
2022-11-24 13:48:11 +0300

    Fix sequence email creation

--- api/src/resources/application/application.routes.ts
+++ api/src/resources/application/application.routes.ts
@@ -5,11 +5,13 @@ import getApplication from './actions/get-application';
 import getFeatures from './actions/get-features';
 import createPipeline from './actions/create-pipeline';
 import createSequence from './actions/create-sequence';
+import createSequenceEmail from './actions/create-sequence-email';
 
 const privateRoutes = routeUtil.getRoutes([
   createFeature,
   createPipeline,
   createSequence,
+  createSequenceEmail,
   getApplication,
   getFeatures,
 ]);
