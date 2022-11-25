6eb6dd6f5b8bd19faa60702c3c745dc3653e6d03
VeronikaUlyanchik <veronikaullll1998@gmail.com>
2022-11-25 14:49:55 +0300

    Get seqs by pipeline

--- api/src/resources/sequence/sequence.routes.ts
+++ api/src/resources/sequence/sequence.routes.ts
@@ -1,9 +1,11 @@
 import { routeUtil } from 'utils';
 import sequenceUpdate from './actions/sequence-update';
+import getSequences from './actions/get-sequences';
 
 
 const privateRoutes = routeUtil.getRoutes([
   sequenceUpdate,
+  getSequences,
 ]);
 
 export default {
