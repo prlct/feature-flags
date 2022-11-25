9f79b84a833481db3a8b8430f21656ff6775d4ac
VeronikaUlyanchik <veronikaullll1998@gmail.com>
2022-11-25 15:28:02 +0300

    Add removal pipeline

--- api/src/resources/pipeline/pipeline.routes.ts
+++ api/src/resources/pipeline/pipeline.routes.ts
@@ -1,10 +1,12 @@
 import { routeUtil } from 'utils';
 import update from './actions/pipeline-update';
 import getPipelines from './actions/get-pipelines';
+import removePipeline from './actions/remove-pipeline';
 
 const privateRoutes = routeUtil.getRoutes([
   update,
   getPipelines,
+  removePipeline,
 ]);
 
 export default {
