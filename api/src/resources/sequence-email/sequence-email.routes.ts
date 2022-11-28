1d3739afefd47bba2ed81370b03f56988e6a165f
VeronikaUlyanchik <veronikaullll1998@gmail.com>
2022-11-28 14:49:04 +0300

    Add removal email seq

--- api/src/resources/sequence-email/sequence-email.routes.ts
+++ api/src/resources/sequence-email/sequence-email.routes.ts
@@ -1,11 +1,13 @@
 import { routeUtil } from 'utils';
 import update from './actions/update-sequence-email';
 import getSequenceEmails from './actions/get-sequence-emails';
+import removeSequenceEmail from './actions/remove-sequence-email';
 
 
 const privateRoutes = routeUtil.getRoutes([
   update,
   getSequenceEmails,
+  removeSequenceEmail,
 ]);
 
 export default {
