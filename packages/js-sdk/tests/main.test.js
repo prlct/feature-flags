bf0a1dbc0b2604011c14fca74e11e68000734842
Uladzimir Shautsou <442126+shevv920@users.noreply.github.com>
2023-01-11 11:32:02 +0300

    Add js-sdk tests (#175)
    * Add jest to js-sdk
    
    * Fix typo

---
+++ packages/js-sdk/tests/main.test.js
@@ -0,0 +1,14 @@
+const { create } = require('../src').default;
+
+describe('SDK instance creation', () => {
+  test('SDK instance not created if no `publicApiKey` provided', () => {
+    expect(() => create({ publicApiKey: '', env: 'development' }))
+      .toThrowError()
+  });
+
+  test('SDK api url sets to production if isDevelopmentApi is not provided', () => {
+    const instance = create({ publicApiKey: 'someKey', env: 'production' });
+
+    expect(instance.apiClient._api.defaults.baseURL).toBe('https://api.growthflags.com')
+  });
+});
