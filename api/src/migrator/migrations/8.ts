f533a5e26f80724389f5983805eff751bcfc45f2
Vladimir Shevtsov <v.shevtsov@paralect.com>
2023-01-18 13:02:28 +0100

    Add companies

--- api/src/migrator/migrations/8.ts
+++ api/src/migrator/migrations/8.ts
@@ -1,18 +1,57 @@
+import moment from 'moment';
+import { generateId } from '@paralect/node-mongo';
+
+import stripeService from 'services/stripe/stripe.service';
 import { Migration } from 'migrator/types';
 import { companyService } from 'resources/company';
-import moment from 'moment';
 import { subscriptionService } from 'resources/subscription';
 
 const migration = new Migration(8, 'Add manually created subscriptions');
 
+const companiesToAddSubscription = [
+  {
+    _id: '63078b286bc29ab70f1b7ddf',
+    name: 'Lighthouse',
+    email: 'andrew@lighthouse.app',
+  },
+  {
+    _id: '63121a20ec55bfa486a86606',
+    name: 'Copysmith',
+    email: 'andrei@copysmith.ai',
+  },
+  {
+    _id: '63288812eacc1a1ec402f2e1',
+    name: 'Bluebanc',
+    email: 'e.chaban@paralect.com',
+  },
+  {
+    _id: '6335af768c6618612eee73ab',
+    name: 'Timeshares',
+    email: 'andrew+timeshares@paralect.com',
+  },
+  {
+    _id: '635006e4a0a97307752626d8',
+    name: 'GoLance',
+    email: 'andrew+golance@paralect.com',
+  },
+  {
+    _id: '636a1aae78537ba88eee35ac',
+    name: 'Paralect World',
+    email: 'a.kukharenko+pworld@paralect.com',
+  },
+];
+
 migration.migrate = async () => {
-  const companyIds = await companyService.distinct('_id', { stripeId: { $exists: false } }  );
+  const customers: {
+    [key in string]: string
+  } = {};
 
-  const customers = companyIds.reduce((acc: any, item: string) => ({
-    ...acc, [item]: (Math.random() + 1).toString(36).substring(7), 
-  }), {});
+  for (const { email, _id: companyId } of companiesToAddSubscription) {
+    const { id: stripeId } = await stripeService.customers.create({ email });
+    customers[companyId] = stripeId;
+  }
 
-  const updateCompanyList = companyIds.map((companyId: string) => ({
+  const updateCompanyList = companiesToAddSubscription.map(({ _id: companyId }) => ({
     updateOne: {
       filter: {
         _id: companyId,
@@ -27,29 +66,28 @@ migration.migrate = async () => {
 
   await companyService.atomic.bulkWrite(updateCompanyList);
 
-  const addSubscriptionsList = companyIds.map((companyId: string) => ({
-    insertOne: { 
-      companyId: companyId,
-      customer: customers[companyId],
-      subscriptionId: 'pro-manual',
-      planId: 'price_1LnMIOKu55YRO0mahVfG4UKl',
-      productId: 'prod_',
-      status: 'active',
-      subscriptionLimits: {
-        emails: 60000,
-        mau: 100000,
-        pipelines: null,
-        users: null,
-      },
-      name: 'pro',
-      interval: 'year',
-      startDate: Number(moment().format('x')),
-      endDate: Number(moment().add(1, 'y').format('x')),
-      cancelAtPeriodEnd: true,
-    }, 
+  const addSubscriptionsList = companiesToAddSubscription.map(({ _id: companyId }) => ({
+    _id: generateId(),
+    companyId: companyId,
+    customer: customers[companyId],
+    subscriptionId: 'pro-manual',
+    planId: 'price_1LnMIOKu55YRO0mahVfG4UKl',
+    productId: 'prod_',
+    status: 'active',
+    subscriptionLimits: {
+      emails: 60000,
+      mau: 100000,
+      pipelines: null,
+      users: null,
+    },
+    name: 'pro',
+    interval: 'year',
+    startDate: Number(moment().format('x')),
+    endDate: Number(moment().add(1, 'y').format('x')),
+    cancelAtPeriodEnd: true,
   }));
 
-  await subscriptionService.atomic.bulkWrite(addSubscriptionsList);
+  await subscriptionService.insertMany(addSubscriptionsList);
 };
 
 export default migration;
