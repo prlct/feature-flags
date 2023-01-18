009c61fbb6936353acb8777cc4cad8457604d3ef
Daria Kotova <85902937+Divakotova@users.noreply.github.com>
2023-01-18 15:03:49 +0300

    Add manually created subscriptions (#177)
    * add 8 migrate
    
    * fix updating collections
    
    * remove unused import
    
    * Add companies
    
    Co-authored-by: Vladimir Shevtsov <v.shevtsov@paralect.com>

--- api/src/resources/subscription/subscription.schema.ts
+++ api/src/resources/subscription/subscription.schema.ts
@@ -1,15 +1,25 @@
 import Joi from 'joi';
 
+const SubscriptionLimitsSchema = {
+  emails: Joi.number().allow(null).default(null),
+  mau: Joi.number().required(),
+  pipelines: Joi.number().allow(null).default(null),
+  users: Joi.number().allow(null).default(null),
+};
+
 const schema = Joi.object({
   _id: Joi.string().required(),
+  companyId: Joi.string().required(),
   subscriptionId: Joi.string().required(),
   planId: Joi.string().required(),
   productId: Joi.string().required(),
   customer: Joi.string().required(),
   status: Joi.string().required(),
+  subscriptionLimits: SubscriptionLimitsSchema,
+  name: Joi.string().allow('').default(''),
   interval: Joi.string().required(),
-  startDate: Joi.date().required(),
-  endDate: Joi.date().required(),
+  startDate: Joi.number().required(),
+  endDate: Joi.number().required(),
   cancelAtPeriodEnd: Joi.bool().required(),
   createdOn: Joi.date(),
   updatedOn: Joi.date(),
