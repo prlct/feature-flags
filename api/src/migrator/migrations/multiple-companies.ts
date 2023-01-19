ea5d5cd9fd1fce63fdc347051e2ff47844ed8a60
Vladimir Shevtsov <v.shevtsov@paralect.com>
2023-01-19 14:41:45 +0100

    Add permissions to the migration

--- api/src/migrator/migrations/multiple-companies.ts
+++ api/src/migrator/migrations/multiple-companies.ts
@@ -35,6 +35,7 @@ migration.migrate = async () => {
   ]);
 
   const [adminUpdates, companyUpdates] = adminsToUpdate.reduce((acc, adminWithCompany) => {
+    const isCompanyOwner = adminWithCompany.ownCompanyId === adminWithCompany.company._id;
     return [[...acc[0], {
       updateOne: {
         filter: { _id: adminWithCompany._id, companies: { $exists: false } },
@@ -51,6 +52,13 @@ migration.migrate = async () => {
               },
             ],
             currentApplicationId: adminWithCompany.applicationIds[0],
+            permissions: {
+              [adminWithCompany.company._id]: {
+                manageMembers: isCompanyOwner,
+                managePayments: isCompanyOwner,
+                manageSenderEmails: isCompanyOwner,
+              },
+            },
           },
         },
         upsert: false,
