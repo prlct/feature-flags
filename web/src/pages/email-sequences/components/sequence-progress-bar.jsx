6750c292685205d1a6ab9c07cbcb9b81596334fc
Vladimir Shevtsov <v.shevtsov@paralect.com>
2023-02-06 12:00:04 +0100

    Change sequence analytics styles

--- web/src/pages/email-sequences/components/sequence-progress-bar.jsx
+++ web/src/pages/email-sequences/components/sequence-progress-bar.jsx
@@ -4,22 +4,41 @@ import { useMediaQuery } from '@mantine/hooks';
 
 const SequenceProgressBar = ({ completed, total, dropped }) => {
   const matches = useMediaQuery('(max-width: 768px)');
-  const completedSection = { color: 'green', value: (completed / total) * 100 };
-  const droppedSection = { color: 'red', value: (dropped / total) * 100 };
+  const convertedColor = '#734AB7';
+  const droppedColor = '#AE8EE4';
+  const mainColor = '#734AB7';
+
+  const completedPercentage = (completed / total) * 100;
+  const droppedPercentage = (dropped / total) * 100;
+
+  const completedSection = { color: convertedColor, value: completedPercentage };
+  const droppedSection = { color: droppedColor, value: droppedPercentage };
   const sections = [completedSection, droppedSection];
 
   return (
     <Stack spacing={matches ? 8 : 18}>
-      <Progress sections={sections} size={4} color="#734AB7" sx={{ backgroundColor: 'rgba(115, 74, 183, 0.2)' }} />
+      <Progress sections={sections} size={8} color={mainColor} sx={{ backgroundColor: 'rgba(115, 74, 183, 0.2)' }} />
       <Group position="apart" px={4}>
-        <Text size="sm" sx={(theme) => ({ color: theme.colors.gray[4] })}>
-          {`Converted: ${completed}`}
-        </Text>
-        <Text size="sm" sx={(theme) => ({ color: theme.colors.gray[4] })}>
-          {`Dropped: ${dropped}`}
-        </Text>
-        <Text size="sm" sx={(theme) => ({ color: theme.colors.gray[4] })}>
-          {`Total: ${total}`}
+        <Group spacing={0}>
+          <Text size="sm">
+            {`${completedPercentage.toFixed(0)}%,`}
+            &nbsp;
+          </Text>
+          <Text weight="bold" size="sm">
+            {completed}
+          </Text>
+        </Group>
+        <Group spacing={0}>
+          <Text size="sm">
+            {`${droppedPercentage.toFixed(0)}%,`}
+            &nbsp;
+          </Text>
+          <Text weight="bold" size="sm">
+            {dropped}
+          </Text>
+        </Group>
+        <Text size="sm">
+          {total}
         </Text>
       </Group>
     </Stack>
