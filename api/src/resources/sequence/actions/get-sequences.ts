671fc418575e20947fbe834e0320aa38a5df58f9
Vladimir Shevtsov <v.shevtsov@paralect.com>
2023-02-08 17:10:46 +0100

    Fix sequence total

--- api/src/resources/sequence/actions/get-sequences.ts
+++ api/src/resources/sequence/actions/get-sequences.ts
@@ -4,6 +4,7 @@ import sequenceService from 'resources/sequence/sequence.service';
 import { AppKoaContext, AppRouter } from 'types';
 import pipelineAccess from 'resources/pipeline/middlewares/pipeline-access';
 import { validateMiddleware } from 'middlewares';
+import { DATABASE_DOCUMENTS } from '../../../app.constants';
 
 const schema = Joi.object({
   pipelineId: Joi.string().trim().required(),
@@ -16,7 +17,25 @@ type ValidatedData = {
 const handler = async (ctx: AppKoaContext<ValidatedData>) => {
   const { pipelineId } = ctx.validatedData;
 
-  const { results: sequences } = await sequenceService.find({ pipelineId,  deletedOn: { $exists: false } });
+  const sequences = await sequenceService.aggregate([
+    { $match: { pipelineId,  deletedOn: { $exists: false } } },
+    { $lookup: {
+      from: DATABASE_DOCUMENTS.PIPELINE_USERS,
+      localField: '_id',
+      foreignField: 'sequences._id',
+      as: 'users',
+    } },
+    {
+      $addFields: {
+        total: {
+          $size: '$users',
+        },
+      },
+    },
+    {
+      $unset: 'users',
+    },
+  ]);
 
   ctx.body = sequences;
 };
