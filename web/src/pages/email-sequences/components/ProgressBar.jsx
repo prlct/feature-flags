eca0d2eb686110833f59786e02d29d53d6c915d6
Vladimir Shevtsov <v.shevtsov@paralect.com>
2023-02-10 16:26:38 +0100

    Add sequence analytics tooltips

--- web/src/pages/email-sequences/components/ProgressBar.jsx
+++ web/src/pages/email-sequences/components/ProgressBar.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-import { Box, Group, Progress, Stack, Text } from '@mantine/core';
+import { Box, Group, Progress, Stack, Text, Tooltip } from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
 import { useMemo } from 'react';
 
@@ -16,6 +16,7 @@ const DoubleSectionProgressBar = (props) => {
     secondaryText,
     totalText,
     isCompact,
+    tooltips,
   } = props;
   const matches = useMediaQuery('(max-width: 768px)');
 
@@ -45,33 +46,39 @@ const DoubleSectionProgressBar = (props) => {
     <Stack spacing={matches ? 8 : 18}>
       <Progress sections={sections} size={8} color={totalColor} sx={{ backgroundColor: 'rgba(115, 74, 183, 0.2)' }} />
       <Box sx={{ display: 'flex', flexDirection: isCompact ? 'row' : 'column', justifyContent: isCompact ? 'space-between' : 'unset' }}>
-        <Group spacing={4}>
-          {renderLegendCircle(primaryColor)}
-          {!isCompact ? primaryText : null}
-          <Text size="sm">
-            {`${primaryPercentage.toFixed(0)}%,`}
-          </Text>
-          <Text weight="bold" size="sm">
-            {primaryCount}
-          </Text>
-        </Group>
-        <Group spacing={4}>
-          {renderLegendCircle(secondaryColor)}
-          {!isCompact ? secondaryText : null}
-          <Text size="sm">
-            {`${secondaryPercentage.toFixed(0)}%,`}
-          </Text>
-          <Text weight="bold" size="sm">
-            {secondaryCount}
-          </Text>
-        </Group>
-        <Group spacing={4}>
-          {renderLegendCircle(totalColor)}
-          {!isCompact ? totalText : null}
-          <Text size="sm" weight={600}>
-            {total}
-          </Text>
-        </Group>
+        <Tooltip label={tooltips.primary} position="top" withArrow disabled={!tooltips.primary}>
+          <Group spacing={4}>
+            {renderLegendCircle(primaryColor)}
+            {!isCompact ? primaryText : null}
+            <Text size="sm">
+              {`${primaryPercentage.toFixed(0)}%,`}
+            </Text>
+            <Text weight="bold" size="sm">
+              {primaryCount}
+            </Text>
+          </Group>
+        </Tooltip>
+        <Tooltip label={tooltips.secondary} position="top" withArrow disabled={!tooltips.secondary}>
+          <Group spacing={4}>
+            {renderLegendCircle(secondaryColor)}
+            {!isCompact ? secondaryText : null}
+            <Text size="sm">
+              {`${secondaryPercentage.toFixed(0)}%,`}
+            </Text>
+            <Text weight="bold" size="sm">
+              {secondaryCount}
+            </Text>
+          </Group>
+        </Tooltip>
+        <Tooltip label={tooltips.total} position="top" withArrow disabled={!tooltips.total}>
+          <Group spacing={4}>
+            {renderLegendCircle(totalColor)}
+            {!isCompact ? totalText : null}
+            <Text size="sm" weight={600}>
+              {total}
+            </Text>
+          </Group>
+        </Tooltip>
       </Box>
     </Stack>
   );
@@ -88,6 +95,11 @@ DoubleSectionProgressBar.propTypes = {
   secondaryText: PropTypes.string,
   totalText: PropTypes.string,
   isCompact: PropTypes.bool,
+  tooltips: PropTypes.shape({
+    primary: PropTypes.string,
+    secondary: PropTypes.string,
+    total: PropTypes.string,
+  }),
 };
 
 DoubleSectionProgressBar.defaultProps = {
@@ -98,6 +110,11 @@ DoubleSectionProgressBar.defaultProps = {
   primaryText: '',
   secondaryText: '',
   totalText: '',
+  tooltips: {
+    primary: '',
+    secondary: '',
+    total: '',
+  },
 };
 
 export default DoubleSectionProgressBar;
